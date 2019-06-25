eded5d70d711c9b9e99462c31f7fb66ad5be4e1f
nouname <darymadry@icloud.com>
2019-06-25 21:13:20 +0300

    f

--- main.cpp
+++ main.cpp
@@ -132,6 +132,8 @@ char start() {
         board->load();
         board->display();
         move(player);
+        board->load();
+        board->display();
         if (!wait(rival_move, "Ожидание хода игрока...")) {
             cout << "Потеряна связь с игороком." << endl;
             return 'T';
