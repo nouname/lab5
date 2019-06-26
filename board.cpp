e3a5aea0644a4427372e9d98008b8cd821da4ae7
nouname <darymadry@icloud.com>
2019-06-26 10:35:53 +0300

    f

--- board.cpp
+++ board.cpp
@@ -87,7 +87,7 @@ char Board::win(char player) {
 
         count = 0;
         for (i = 1; i <= M; i++)
-            if (*matrix[M - i][M - i] == player)
+            if (*matrix[M - i][i - 1] == player)
                 count++;
         if (count >= M)
             return player;
